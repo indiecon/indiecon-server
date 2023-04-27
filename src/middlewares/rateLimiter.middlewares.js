e94d269ff5c5178d2f233a4855d5e65fbd72cb85
AdityaKG-169 <aditya.kg169@gmail.com>
2023-04-27 10:31:13 +0530

    feat: change rate limit

--- src/middlewares/rateLimiter.middlewares.js
+++ src/middlewares/rateLimiter.middlewares.js
@@ -3,7 +3,7 @@ const rateLimit = require('express-rate-limit');
 // config for rate limiter
 const rateLimiter = rateLimit({
 	windowMs: 5 * 60 * 1000, // 5 minutes
-	max: 50, // Limit each IP to 50 requests per `window` (here, per 5 minutes)
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 5 minutes)
 	standardHeaders: false, // Return rate limit info in the `RateLimit-*` headers
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 	message: {
